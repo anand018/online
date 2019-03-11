c33a92d22351e1bf805a6a951cb22851ea193015
Anand <anandpsingh7@gmail.com>
2019-03-11 22:04:22 +0530

    written looger for HomeController

--- src/main/java/com/mail/controllers/HomeController.java
+++ src/main/java/com/mail/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.mail.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -8,11 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+	private static final Logger logger = Logger.getLogger(HomeController.class);
+
 	@GetMapping("/home.htm")
 	public void showHome(ModelMap map) {
 
+		if (logger.isDebugEnabled())
+			logger.debug("Enter into customer home controller method");
+
 		String user = SecurityContextHolder.getContext().getAuthentication().getName();
 		map.addAttribute("user", "welcome " + user);
+		if (logger.isDebugEnabled())
+			logger.debug("Exit from customer home controller method");
 
 	}
 
